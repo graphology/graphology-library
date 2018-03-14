a6fcfad63b57fdf8763746ab51ce7896f4bc5531
Yomguithereal <guillaumeplique@gmail.com>
2018-03-14 18:03:38 +0100

    Library files

--- browser.js
+++ browser.js
@@ -4,3 +4,18 @@
  *
  * Library endpoint for the browser.
  */
+exports.assertions = require('./assertions');
+exports.communitiesLouvain = require('./communities-louvain');
+exports.components = require('./components');
+exports.generators = require('./generators');
+exports.hits = require('./hits');
+exports.layout = require('./layout');
+exports.layoutForceAtlas2 = require('./layout-forceatlas2');
+exports.metrics = require('./metrics');
+exports.operators = require('./operators');
+exports.shortestPath = require('./shortest-path');
+exports.utils = require('./utils');
+
+// Browser specific
+exports.FA2Layout = require('graphology-layout-forceatlas2/worker');
+exports.gexf = require('graphology-gexf/browser');
--- index.js
+++ index.js
@@ -4,3 +4,15 @@
  *
  * Library endpoint.
  */
+exports.assertions = require('./assertions');
+exports.communitiesLouvain = require('./communities-louvain');
+exports.components = require('./components');
+exports.generators = require('./generators');
+exports.gexf = require('./gexf');
+exports.hits = require('./hits');
+exports.layout = require('./layout');
+exports.layoutForceAtlas2 = require('./layout-forceatlas2');
+exports.metrics = require('./metrics');
+exports.operators = require('./operators');
+exports.shortestPath = require('./shortest-path');
+exports.utils = require('./utils');
